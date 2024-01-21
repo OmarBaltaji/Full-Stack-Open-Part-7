891edadc386115512783663fa62572f67bc1f01c
Omar Baltaji <omr.baltaji@gmail.com>
2024-01-21 12:39:16 +0200

    7.17: Navigation

--- BlogApp-ReactQuery/src/components/Navbar.js
+++ BlogApp-ReactQuery/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useUserDispatch, useUserValue } from "../contexts";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const user = useUserValue();
@@ -14,13 +15,17 @@ const Navbar = () => {
       {user && 
         <>
           <div className="navbar">
-            <button onClick={handleLogout}>Logout</button>
+            <div>
+              <Link to='/' className="mr-1">Blogs</Link>
+              <Link to='/users'>Users</Link>
+            </div>
+            <div>
+              <strong className="mr-1">Logged in as {user.name}</strong>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
           </div>
           <div className="container">
-            <h2>blogs</h2>
-            <p>
-              <strong>Logged in as {user.name}</strong>
-            </p>
+            <h2>blog app</h2>
           </div>
         </>
       }
